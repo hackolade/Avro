fffd3f5aac53002ea3f29bb6a0d097281f788bc7
Nazar Kovtun <nazar.kovtun@hackolade.com>
2023-11-30 15:12:15 +0200

    HCK-3867: added check for absent schemaTopic when extracting namespace

--- reverse_engineering/helpers/generalHelper.js
+++ reverse_engineering/helpers/generalHelper.js
@@ -1,6 +1,6 @@
 const { dependencies } = require('../../shared/appDependencies');
 
-const getNamespaceFromSchemaTopic = (schemaTopic) => schemaTopic.split('.').slice(0, -1).join('.')
+const getNamespaceFromSchemaTopic = (schemaTopic) => schemaTopic?.split('.').slice(0, -1).join('.')
 const getName = ({ name: fullName }) => dependencies.lodash.last((fullName || '').split('.'));
 const getNamespace = ({ name: fullName, namespace, schemaTopic }) => (fullName || '').split('.').slice(0, -1).join('.') || namespace || getNamespaceFromSchemaTopic(schemaTopic);
 const EMPTY_NAMESPACE = '#emptyNamespace';
